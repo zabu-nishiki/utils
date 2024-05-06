b02ad237e0ffc32fa44b2a526ba8594f30367575
zabu-nishiki <34130049+zabu-nishiki@users.noreply.github.com>
2024-05-06 22:47:07 +0900

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,6 @@
+def bubble_sort(array):
+	array_length = len(array)
+	for i_itr in range(array_length):
+        for j_itr in range(array_length-i_itr-1):
+            if array[j_itr] > array[j_itr+1]: array[j_itr], array[j_itr+1] = array[j_itr+1], array[j_itr]
+    return array
