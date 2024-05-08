1b2414b1f38eed6dc291be2d1511056a0a90a693
zabu-nishiki <34130049+zabu-nishiki@users.noreply.github.com>
2024-05-08 13:53:39 +0900

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -4,7 +4,7 @@ def bubble_sort(array):
 		for j_itr in range(array_length-i_itr-1):
 			if array[j_itr] > array[j_itr+1]: array[j_itr], array[j_itr+1] = array[j_itr+1], array[j_itr]
 	return array
-#############################################
+##########################################################################################
 def selection_sort(array):
 	array_length = len(array)
 	for i_itr in range(array_length):
@@ -13,7 +13,7 @@ def selection_sort(array):
 			if array[j_itr] < array[min_idx]: min_idx = j_itr
 		array[i_itr], array[min_idx] = array[min_idx], array[i_itr]
 	return array
-
+##########################################################################################
 def upheap(array, child_idx):
 	while child_idx!=0:
 		parent_idx = int((child_idx-1)/2)
